6ec256c2f35cacb18d615912c4015eca507d9cbb
SMKrizan <smkrizan@gmail.com>
2021-01-17 14:05:32 -0600

    jquery issue solved!

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,9 @@
 // webpack.build.js is not strictly necessary, but will allow us to add specifications for the webpack build.
 const path = require('path');
 const webpack = require('webpack');
-require('bootstrap');
 
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: require.resolve('jquery'),
-                loader: 'expose-loader',
-                options: {
-                    exposes: {
-                        globalName: "$",
-                        override: true
-
-                    },
-                }
-            }
-        ],
-    },
-
     // assigns root of bundle and beginning of dependency graph (default is ./src/index.js)
     entry: './assets/js/script.js',
     // webpack will bundle code at entry point and output into folder specified folder (default is ./dist/main.js)
