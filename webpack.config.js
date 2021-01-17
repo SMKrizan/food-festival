f292abee80b45232f5e15457811f4528dc857938
SMKrizan <smkrizan@gmail.com>
2021-01-16 21:37:33 -0600

    jquery TypeError persists

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,36 @@ const path = require('path');
 const webpack = require('webpack');
 require('bootstrap');
 
+
 module.exports = {
-    // root of bundle and beginning of dependency graph
+    module: {
+        rules: [
+            {
+                test: require.resolve('jquery'),
+                loader: 'expose-loader',
+                options: {
+                    exposes: {
+                        globalName: "$",
+                        override: true
+
+                    },
+                }
+            }
+        ],
+    },
+
+    // assigns root of bundle and beginning of dependency graph (default is ./src/index.js)
     entry: './assets/js/script.js',
-    // webpack will bundle code at entry point and output into folder we specify
+    // webpack will bundle code at entry point and output into folder specified folder (default is ./dist/main.js)
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
     // default is 'production' mode, which will minify code automatically; in this case we do not want this feature
-    plugins:[
+    plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
-            jQuery: "jquery"
+            jQuery: "jquery",
         }),
     ],
     mode: 'development'
